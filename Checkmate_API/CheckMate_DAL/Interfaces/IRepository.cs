3f323bffafc1ec1410090bed24ed2ea8c88a7ddd
Benjamin Malemprée <benjamin_malempree@hotmail.com>
2022-08-08 11:38:18 +0200

    Création des interfaces "IEntity" et "IRepository"

---
+++ Checkmate_API/CheckMate_DAL/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckMate_DAL.Interfaces
+{
+    public interface IRepository<Tentity, TKey> where Tentity : IEntity<TKey>
+    {
+        // Implémenter ici les méthodes du CRUD
+    }
+}
