a9f8b0389c9bfb8edcad4a7c87b95af8fd993f6c
Benjamin Malemprée <benjamin_malempree@hotmail.com>
2022-08-11 08:55:02 +0200

    Création des dossiers Exceptions dans la BLL et la DAL. Création des classe My Exceptions qui contiennent des exceptions Custom dans la DAL et la BLL. Ajout d'un Try / Catch dans la méthode ConnectionOpen dans la classe DataAccess afin d'envoyer une exception dans le cas d'un problème de connexion au serveur.

---
+++ CheckMate_DAL/Exceptions/MyDALExceptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckMate_DAL.Exceptions
+{
+    public class MyDALExceptions { }
+    public class ConnectionFailedException : Exception 
+    {
+        public ConnectionFailedException(string message) : base(message) { }
+    }
+}
